58715f46398b633197ca618cdd534c4994e85a0c
maxerkmar <max.erkmar@iths.se>
2024-09-10 21:59:33 +0200

    some changes

--- js/add-city.js
+++ js/add-city.js
@@ -1,21 +1,35 @@
-document.getElementById('add-city-form').addEventListener('submit', function(event) {
+document
+  .getElementById("add-city-form")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const cityName = document.getElementById('city-name').value;
-    const cityImage = document.getElementById('city-image').value;
-    const cityDescription = document.getElementById('city-description').value;
+    const cityName = document.getElementById("city-name").value;
+    const cityImage = document.getElementById("city-image").value;
+    const cityDescription = document.getElementById("city-description").value;
+    const cityCountry = document.getElementById("city-country").value;
 
     const newCity = {
-        name: cityName,
-        image: cityImage,
-        description: cityDescription
+      name: cityName,
+      image: cityImage,
+      description: cityDescription,
     };
 
-    const cities = JSON.parse(localStorage.getItem('cities')) || [];
+    const citiesByCountry =
+      JSON.parse(localStorage.getItem("citiesByCountry")) || {};
+    const cities = citiesByCountry[cityCountry] || [];
+
     cities.push(newCity);
-    localStorage.setItem('cities', JSON.stringify(cities));
 
-    window.location.href = 'france.html';
-});
+    citiesByCountry[cityCountry] = cities;
+    localStorage.setItem("citiesByCountry", JSON.stringify(citiesByCountry));
 
-
+    if (cityCountry == "france") {
+      window.location.href = "france.html";
+    } else if (cityCountry == "usa") {
+      window.location.href = "usa.html";
+    } else if (cityCountry == "italy") {
+      window.location.href = "italy.html";
+    } else {
+      window.location.href = "homepage.html";
+    }
+  });
